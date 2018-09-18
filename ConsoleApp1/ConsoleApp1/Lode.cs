0b21728c8a610d70a28d91b60830eab847b3a636
Martin Hynek <martin.hyn@email.cz>
2018-09-18 10:28:12 +0200

    lode

---
+++ ConsoleApp1/ConsoleApp1/Lode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Lode
+    {
+        public void Ponorka()
+        {
+            Pozice pivot = new Pozice()
+            {
+                PozX = 0,
+                PozY = 0
+            };
+
+        }
+    }
+}
