b334e38ab05fc2180edc393422124e17ac452c42
Martin Hynek <martin.hyn@email.cz>
2018-09-18 09:04:20 +0200

    1.

---
+++ ConsoleApp1/ConsoleApp1/pozice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Pozice
+    {
+        public int PozX;
+        public int PozY;
+    }
+}
