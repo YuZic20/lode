601a35366ef1b69b57ca410917e9b8b582bdf814
Martin Hynek <martin.hyn@email.cz>
2018-09-19 12:27:08 +0200

    lode

--- ConsoleApp1/ConsoleApp1/Lode.cs
+++ ConsoleApp1/ConsoleApp1/Lode.cs
@@ -8,14 +8,38 @@ namespace ConsoleApp1
 {
     class Lode
     {
-        public void Ponorka()
+        enum RotationState
         {
-            Pozice pivot = new Pozice()
-            {
-                PozX = 0,
-                PozY = 0
-            };
-
+            Nahoru,
+            Dolu,
+            Vpravo,
+            Vlevo
         }
+        private string ShipType;
+        private Pozice pivot;
+        private List<Pozice> LodStav1;
+        private List<Pozice> LodStav2;
+        private List<Pozice> LodStav3;
+        private List<Pozice> LodStav4;
+        public Lode (string ShipType, Pozice pivot, List<Pozice> LodStav1, List<Pozice> LodStav2, List<Pozice> LodStav3, List<Pozice> LodStav4)
+        {
+            
+        /*
+        Pozice pivot = new Pozice()
+        {
+            PozX = 0,
+            PozY = 0
+        };
+
+        List<Pozice> LodObsah = new List<Pozice>();
+        LodObsah.Add(new Pozice
+        {
+            PozX = 0,
+            PozY = 0
+        });
+        */
+    }
+
+
     }
 }
